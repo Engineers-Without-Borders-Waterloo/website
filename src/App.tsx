882ab1151ee1d707fcdd7d8941812979a0ca36fc
Vivian Yuan <vivianyuan@eduroam-campus-10-36-218-223.campus-dynamic.uwaterloo.ca>
2026-02-06 17:22:49 -0500

    added basic navbar will modify later

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
 import './App.css'
 import Greeting from './components/Greeting/Greeting.tsx'
+import Navbar from './components/Navbar/Navbar'
 
 function App() {
 
   return (
-    <div style={{maxWidth: 960, margin: '0 auto', padding: 16}}>
-      <h1>Playground</h1>
-      <Greeting/>
-      <Greeting name="Aidan"/>
-      <h1>Hello World!</h1>
-    </div>
+    <>
+      <Navbar />
+      <div style={{maxWidth: 960, margin: '0 auto', padding: 16}}>
+        <h1>Playground</h1>
+        <Greeting/>
+        <Greeting name="Aidan"/>
+        <h1>Hello World!</h1>
+      </div>
+    </>
   )
 }
 
