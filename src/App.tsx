4f25588ff8637dcefc8fee17886df5e84e5cb377
Aidan Ko <aidan.tj.ko@gmail.com>
2026-02-06 17:26:23 -0500

    seeing if there are merge conflicts

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,9 @@
 import './App.css'
-import Greeting from './components/Greeting/Greeting.tsx'
-
+import Card from './components/ui/Card/Card.tsx'
 function App() {
 
   return (
-    <div style={{maxWidth: 960, margin: '0 auto', padding: 16}}>
-      <h1>Playground</h1>
-      <Greeting/>
-      <Greeting name="Aidan"/>
-      <h1>Hello World!</h1>
-    </div>
+    <Card name="Cheese"/>
   )
 }
 
