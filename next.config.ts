c1152c71002e2201d739f76888302cf744c77f54
Shathviki Krishnaraj <shathvikikrishnaraj@eduroam-campus-10-36-57-213.campus-dynamic.uwaterloo.ca>
2026-09-14 21:24:19 -0400

    remove turbopack

--- next.config.ts
+++ next.config.ts
@@ -3,13 +3,13 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export",
   // Keep this if you were using it to fix Turbopack root detection
-  turbopack: {
-    root: process.cwd(),
-  },
+  // turbopack: {
+  //   root: process.cwd(),
+  // },
 
   images: {
     unoptimized: true,
-  },
+  }
 };
 
 export default nextConfig;
